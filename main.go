fc0d8acf69d2b5e76e4cc0db7b6e7460a1af2c08
Raed Abdul Majeed <100485450+raedmajeed@users.noreply.github.com>
2024-02-22 22:05:51 +0530

    initial commit

--- main.go
+++ main.go
@@ -7,20 +7,16 @@ import (
 )
 
 func main() {
-	// configure the songs directory name and port
 	const directory = "files"
 	const port = 8080
 
-	// add a handler for the song files
 	http.Handle("/", addHeaders(http.FileServer(http.Dir(directory))))
 	fmt.Printf("Starting server on %v\n", port)
 	log.Printf("Serving %s on HTTP port: %v\n", directory, port)
 
-	// serve and log errors
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-// addHeaders will act as middleware to give us CORS support
 func addHeaders(h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
